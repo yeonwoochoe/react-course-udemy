bf77d3f0d8ba405c6ea2819d60b893c95ee1e05d
YEONWOO <89890240+yeonwoochoe@users.noreply.github.com>
2022-01-25 18:13:17 +0900

    [feat] components : 컴포지션 개념 (children props)

---
+++ src/components/UI/Card.js
@@ -0,0 +1,8 @@
+import "../UI/Card.css";
+
+function Card(props) {
+  const classes = "card " + props.className;
+  return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;
