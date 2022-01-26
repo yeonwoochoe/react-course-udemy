ea86192c1aa707a0a24720f1fc12f39f083a6096
YEONWOO <89890240+yeonwoochoe@users.noreply.github.com>
2022-01-26 20:20:27 +0900

    [feat] components : State 및 이벤트 동작과 각 컴포넌트 렌더링 연결과 리스트 생성하기

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,9 +1,11 @@
+import React from "react";
+
 import "./ExpenseDate.css";
 
 const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
-  const year = props.date.getFullYear();
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-date">
