15fad170814ab81e4f223b27eedc207dcadde11a
YEONWOO <89890240+yeonwoochoe@users.noreply.github.com>
2022-01-27 14:13:44 +0900

    feat] components : react-style 적용하는 다양한 방법 (inline-style, style-components,css-module)
    css moudle로 진행

---
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Button from "../../UI/Button/Button";
+import style from "./CourseInput.module.css";
+
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => (props.isvalid ? "red" : "black")};
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+//     barckround: ${(props) => (props.isvalid ? "#ffd7d7" : "transparent")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(false);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={`${style["form-control"]} ${!isValid && style.invalid}`}>
+        {/* <label style={{ color: !isVaild ? "red" : "black" }}>Course Goal</label>  인라인 스타일로 적용하는방법 하지만 추천하지 않음*/}
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
