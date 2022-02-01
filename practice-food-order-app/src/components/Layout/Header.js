322d7d40703e96033dc41b8a561096081a5b54d8
YEONWOO <89890240+yeonwoochoe@users.noreply.github.com>
2022-02-01 17:49:29 +0900

    [feat] components : 새로운 프로젝트 연습 food app
    생각보다 어려운 로직....기본개념이 다시 정리해서 연습해야겠다

--- practice-food-order-app/src/components/Layout/Header.js
+++ practice-food-order-app/src/components/Layout/Header.js
@@ -1,16 +1,18 @@
-import React, { Fragment } from "react";
-import mealsImage from "../../assets/meals.jpeg";
-import classes from "./Header.module.css";
-import HeaderCartButton from "./HeaderCartButton";
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>React Meals</h1>
-        <HeaderCartButton />
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
-      <div className={classes["main-image"]}>
-        <img src={mealsImage} alt="식탁 위에 맛있는 음식들이 가득 차있는 이미지" />
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
       </div>
     </Fragment>
   );
