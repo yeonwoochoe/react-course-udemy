83c1afe1fe65f531b1785668ea6fba0171709d49
YEONWOO <89890240+yeonwoochoe@users.noreply.github.com>
2022-02-08 18:19:13 +0900

    [feat] form : 사용자 입력 값 가져오기 및 유효성 관리하기 연습와 커스텀 hook으로 반복코드 줄이기

--- practice-user-form-guide/src/components/SimpleInput.js
+++ practice-user-form-guide/src/components/SimpleInput.js
@@ -1,12 +1,62 @@
+import useInput from "../hooks/use-input";
+
 const SimpleInput = (props) => {
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangedHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetNameInput,
+  } = useInput((value) => value.trim() !== "");
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailChangedHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput,
+  } = useInput((value) => value.includes("@"));
+
+  let formIsValid = false;
+  if (enteredNameIsValid && enteredEmailIsValid) {
+    formIsValid = true;
+  }
+
+  const formSumissionHandler = (event) => {
+    event.preventDefault();
+
+    if (!enteredName.trim() === "") {
+      return; //  //위의 조건을 만족하면 해당 함수를 중단시킴
+    }
+
+    console.log(enteredName);
+
+    // nameInputRef.current.value = ""; 돔의 직접 접근하는것은 좋지 않는 방식! 오로지 리액트만이 돔에 접근하도록 해야함.
+    resetNameInput();
+    resetEmailInput();
+  };
+
+  const nameInputClasses = nameInputHasError ? "form-control invalid" : "form-control";
+
+  const emailInputClasses = emailInputHasError ? "form-control invalid" : "form-control";
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSumissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" onChange={nameChangedHandler} onBlur={nameBlurHandler} value={enteredName} />
+        {nameInputHasError && <p className="error-text">이름에 공백을 넣지마세요</p>}
+      </div>
+
+      <div className={emailInputClasses}>
+        <label htmlFor="email">Your E-mail</label>
+        <input type="email" id="email" onChange={emailChangedHandler} onBlur={emailBlurHandler} value={enteredEmail} />
+        {emailInputHasError && <p className="error-text">올바른 이메일을 입력하세요</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
