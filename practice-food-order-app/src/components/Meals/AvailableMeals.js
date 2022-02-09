fcd4503b5372203ffa3bd1e33b8ecc6270c57569
yeonwoo choe <chiyy6162@gmail.com>
2022-02-09 21:45:08 +0900

    [feat] data : dummy data 지우고 firebase로 데이터 직접 불러오기

--- practice-food-order-app/src/components/Meals/AvailableMeals.js
+++ practice-food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,44 +1,32 @@
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
-
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem
-      key={meal.id}
-      id={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const respons = await fetch("https://react-http-f1906-default-rtdb.firebaseio.com/meals.json");
+      const responsData = await respons.json();
+
+      const loadedMeals = [];
+      for (const key in responsData) {
+        loadedMeals.push({
+          id: key,
+          name: responsData[key].name,
+          description: responsData[key].description,
+          price: responsData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
 
   return (
     <section className={classes.meals}>
