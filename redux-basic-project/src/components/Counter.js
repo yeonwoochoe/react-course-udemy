8e60d8ae291c51f44d9fb277c2a5a103a95a0b75
yeonwoo <chiyy6162@gmail.com>
2022-02-23 15:33:57 +0900

    [feat] redux :  useSelector, useDispatch 사용해서 카운터 구현하기

--- redux-basic-project/src/components/Counter.js
+++ redux-basic-project/src/components/Counter.js
@@ -1,15 +1,35 @@
 import classes from "./Counter.module.css";
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
 
-  const toggleCounterHandler = () => {};
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
