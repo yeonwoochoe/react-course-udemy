68de930e77ba7ef64f99ca9ccd8174bb46373b9d
yeonwoo <chiyy6162@gmail.com>
2022-03-03 10:00:45 +0900

    [feat] router V5 :  쿼리 매개변수, 중첩 경로, prompt 사용해보기

--- practice-time-to/src/pages/QuoteDetail.js
+++ practice-time-to/src/pages/QuoteDetail.js
@@ -1,16 +1,36 @@
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 import { Fragment } from "react";
-
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Component from "../components/comments/Comments";
 
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Maximilian", text: "Learning React is fun!" },
+];
+
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
+
+  console.log(match);
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found</p>;
+  }
 
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+
+      <Route path={`${match.path}/comments`}>
         <Component />
       </Route>
     </Fragment>
