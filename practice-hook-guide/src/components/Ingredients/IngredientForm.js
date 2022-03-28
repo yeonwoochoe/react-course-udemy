11271e6aee261b7c0c3fffa012b4ff917afc5fde
yeonwoo <chiyy6162@gmail.com>
2022-03-28 18:35:21 +0900

    [feat] database : http 요청보내기

--- practice-hook-guide/src/components/Ingredients/IngredientForm.js
+++ practice-hook-guide/src/components/Ingredients/IngredientForm.js
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
-const IngredientForm = React.memo(props => {
-  const [inputState, setInputState] = useState({ title: '', amount: '' });
+const IngredientForm = React.memo((props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
 
-  const submitHandler = event => {
+  const submitHandler = (event) => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({ title: enteredTitle, amount: enteredAmount });
   };
 
   return (
@@ -20,13 +21,9 @@ const IngredientForm = React.memo(props => {
             <input
               type="text"
               id="title"
-              value={inputState.title}
-              onChange={event => {
-                const newTitle = event.target.value;
-                setInputState(prevInputState => ({
-                  title: newTitle,
-                  amount: prevInputState.amount
-                }));
+              value={enteredTitle}
+              onChange={(event) => {
+                setEnteredTitle(event.target.value);
               }}
             />
           </div>
@@ -35,13 +32,9 @@ const IngredientForm = React.memo(props => {
             <input
               type="number"
               id="amount"
-              value={inputState.amount}
-              onChange={event => {
-                const newAmount = event.target.value;
-                setInputState(prevInputState => ({
-                  amount: newAmount,
-                  title: prevInputState.title
-                }));
+              value={enteredAmount}
+              onChange={(event) => {
+                setEnteredAmount(event.target.value);
               }}
             />
           </div>
--- practice-hook-guide/src/components/Ingredients/Ingredients.js
+++ practice-hook-guide/src/components/Ingredients/Ingredients.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    fetch(
+      "https://react-hooks-update-d1151-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        return response.json(); //바디를 추출해서  json에서 자바스크립트 코드로 전환
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+          //0 이상 1 미만의 부동소숫점 의사 난수.
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    setUserIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+    );
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
