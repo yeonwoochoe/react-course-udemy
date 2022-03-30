d688bd747027d8412c3bdb0dd33b77a17bdcc7fb
yeonwoo <chiyy6162@gmail.com>
2022-03-30 19:44:29 +0900

    [feat] hook : 커스텀훅 사용해보기

--- practice-hook-guide/src/components/Ingredients/IngredientList.js
+++ practice-hook-guide/src/components/Ingredients/IngredientList.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './IngredientList.css';
+import "./IngredientList.css";
 
-const IngredientList = props => {
+const IngredientList = (props) => {
+  console.log("재료 렌더링중");
   return (
     <section className="ingredient-list">
       <h2>Loaded Ingredients</h2>
       <ul>
-        {props.ingredients.map(ig => (
+        {props.ingredients.map((ig) => (
           <li key={ig.id} onClick={props.onRemoveItem.bind(this, ig.id)}>
             <span>{ig.title}</span>
             <span>{ig.amount}x</span>
